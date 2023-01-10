bd5e5161a31d63a3879791d7308495c636ba00b9
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-01-10 18:06:59 +0900

    Update DepenetrationVelocityLimitter.cs

--- Assets/OcsTerrain/Scripts/Excavation/Excavator/DepenetrationVelocityLimitter.cs
+++ Assets/OcsTerrain/Scripts/Excavation/Excavator/DepenetrationVelocityLimitter.cs
@@ -7,9 +7,20 @@ public class DepenetrationVelocityLimitter : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField]
-    private float velocity;
+    private float _velocity;
+
+    [SerializeField]
+    private Transform _targetVelocty;
+
+    [SerializeField]
+    private float _targetVelocityCoeff = 1.0f;
+
+    [SerializeField]
+    private float _offsetVelocity;
 
     private Rigidbody rb;
+    private Vector3 _pos_old;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -17,6 +28,17 @@ public class DepenetrationVelocityLimitter : MonoBehaviour
 
     private void Start()
     {
-        rb.maxDepenetrationVelocity = velocity;
+        rb.maxDepenetrationVelocity = _velocity;
+    }
+
+    private void Update()
+    {
+        if (!_targetVelocty) return;
+        Vector3 pos = _targetVelocty.position;
+        _velocity = (pos - _pos_old).magnitude*_targetVelocityCoeff + _offsetVelocity;
+
+        rb.maxDepenetrationVelocity = _velocity;
+        
+        _pos_old = pos;
     }
 }
